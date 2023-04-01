f5a47ff6783bfa5ce3d12607d4de1b1b7f48481b
mweene <harrisonmweene11@gmail.com>
2023-04-01 02:29:34 +0200

    added javascript, make it interactive

---
+++ js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const item = document.getElementById("text-input");
+const addTask = document.getElementById("add-item");
+const clearAll = document.getElementById("clear-all");
+const checkBox = document.getElementById("checkbox");
+
+addTask.addEventListener("click",() => console.log('muli shani bwantu ba kwa lesa'));
