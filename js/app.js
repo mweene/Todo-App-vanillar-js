3c30d68b2e7bae22a69a952a9027325b2d36d497
mweene <harrisonmweene11@gmail.com>
2023-04-02 05:51:25 +0200

    new task can be added now, but not custom tasks

--- js/app.js
+++ js/app.js
@@ -3,6 +3,37 @@
 const item = document.getElementById("text-input");
 const addTask = document.getElementById("add-item");
 const clearAll = document.getElementById("clear-all");
-const checkBox = document.getElementById("checkbox");
+//const checkBox = document.getElementById("checkbox");
+const list = document.querySelector(".task-list");
 
-addTask.addEventListener("click",() => console.log('muli shani bwantu ba kwa lesa'));
+const createTaskItem = function() {
+    const li = document.createElement('li');
+    const div = document.createElement('div');
+    const input = document.createElement('input');
+    const innerDiv = document.createElement('div');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+
+    li.classList.add('item');
+    div.classList.add('check');
+    input.type = 'checkbox';
+    input.classList.add('status');
+    input.classList.add('checkbox');
+    innerDiv.classList.add('text');
+
+    h3.innerText = 'Learning some Vanilla JS.';
+    p.innerText = 'Thursday, 05:22 AM';
+
+    li.appendChild(div);
+    div.appendChild(input);
+    innerDiv.appendChild(h3).appendChild(p);
+    div.appendChild(innerDiv);
+
+    list.appendChild(li);
+}
+
+addTask.addEventListener('click', createTaskItem);
+
+clearAll.addEventListener('click', () => {
+    list.innerHTML = '';
+})
